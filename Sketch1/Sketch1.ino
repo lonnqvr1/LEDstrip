d7abb50bec3985be96b8ba51e58eb2cd99a61d8b
Robin <Robin@JENNIFER>
2016-08-29 20:50:17 +0300

    Add project files.

---
+++ Sketch1/Sketch1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		LEDstrip.ino
+ Created:	29/8/2016 8:37:17 PM
+ Author:	Robin
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
