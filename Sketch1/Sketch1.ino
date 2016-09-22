9ea3818696e8703cc06791cf1f7fe28095e9642c
Robin <Robin@JENNIFER>
2016-09-22 21:19:36 +0300

    Added led strip name

--- Sketch1/Sketch1.ino
+++ Sketch1/Sketch1.ino
@@ -2,6 +2,7 @@
  Name:		LEDstrip.ino
  Created:	29/8/2016 8:37:17 PM
  Author:	Robin
+ This code is for a 1M WS2812B 5050 RGB Non-Waterproof 60 LED Strip
 */
 
 // the setup function runs once when you press reset or power the board
@@ -11,5 +12,6 @@ void setup() {
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+  //habla habla
+	//h‰‰ miten t‰‰ toimii
 }
